2cf9db81b6685cb03e6279c546739303bfab0d76
vlad-berd <9vladbusiness@gmail.com>
2026-03-16 23:49:30 +0300

    Update test_constructor_page.py
    Fixed the import name from the data module

--- tests/test_constructor_page.py
+++ tests/test_constructor_page.py
@@ -4,12 +4,12 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from curl import *
 from locators import Locators
-from data import ExpectedValueConstructor as EVC
+from data import ExpectedValueConstructorPage as EVCP
 
 
 class TestConstructorMenuNavigation:
     
-    @pytest.mark.parametrize("tab, expected_value", [(Locators.TAB_SAUCES, EVC.text_tab_sauces), (Locators.TAB_TOPPINGS, EVC.text_tab_toppings)])
+    @pytest.mark.parametrize("tab, expected_value", [(Locators.TAB_SAUCES, EVCP.text_tab_sauces), (Locators.TAB_TOPPINGS, EVCP.text_tab_toppings)])
     def test_section_navigation_success(self, driver, tab, expected_value):
         """Проверка переходов между разделами"""
         driver.get(BASE_URL)
@@ -25,6 +25,6 @@ class TestConstructorMenuNavigation:
         driver.find_element(*Locators.TAB_SAUCES).click()
 
         driver.find_element(*Locators.TAB_BREADS).click()
-        is_text_in_element = WebDriverWait(driver, 3).until(EC.text_to_be_present_in_element(Locators.SECTION_SELECTED_CONSTRUCTOR, EVC.text_tab_breads))
+        is_text_in_element = WebDriverWait(driver, 3).until(EC.text_to_be_present_in_element(Locators.SECTION_SELECTED_CONSTRUCTOR, EVCP.text_tab_breads))
 
         assert is_text_in_element == True
